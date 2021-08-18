446e0ad84d4b1a76f33e7c3db90a5dbe4b37afff
frandrew90 <frandrewjs@gmail.com>
2021-08-18 21:27:20 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+// const ingListRef = document.querySelector('#ingredients');
+
+// ingredients.forEach(element => {
+//   const newLiRef = document.createElement('li');
+//   newLiRef.textContent = element;
+//   ingListRef.append(newLiRef);
+// });
+
+const ingListRef = document.querySelector('#ingredients');
+
+const listArr = ingredients.map(element => {
+  const newLiRef = document.createElement('li');
+  newLiRef.textContent = element;
+  return newLiRef;
+});
+
+ingListRef.append(...listArr);
