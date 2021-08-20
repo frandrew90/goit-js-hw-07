2ba87922c8f333d421db11ddfae6a1ccc6b7dee0
frandrew90 <frandrewjs@gmail.com>
2021-08-20 16:29:59 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -7,3 +7,25 @@
 // <input id="font-size-control" type="range" />
 // <br />
 // <span id="text">Абракадабра!</span>
+
+const refsContainer = document.querySelector('body');
+
+const refs = {
+  input: refsContainer.querySelector('#font-size-control'),
+
+  text: refsContainer.querySelector('#text'),
+};
+
+refs.input.addEventListener('input', onChangeRange);
+
+// function onChangeRange(e) {
+//   console.log(e);
+//   console.log(e.target.value);
+//   refs.text.style.fontSize = '${Number(e.target.value)}px';
+// }
+
+function onChangeRange(e) {
+  console.log(e);
+  console.log(e.target.value);
+  refs.text.style.fontSize = `${e.target.value}px`;
+}
